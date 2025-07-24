5bbacdc04f93e6bf2a731e4dabd62735c850ee84
Einsy <eins.yeager@gmail.com>
2025-07-24 12:15:26 +0400

    fix err

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,14 +3,16 @@ import { ContextMenu } from "@/components/widgets/ContextMenu";
 import store from "@/lib/store";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "nativewind";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import "../assets/css/global.css";
 
 export default function Layout() {
+  const { colorScheme } = useColorScheme();
   return (
     <SafeAreaView className="flex-1 app">
-      <StatusBar />
+      <StatusBar style={colorScheme === "light" ? "dark" : "light"} />
       <Provider store={store}>
         <Stack>
           <Stack.Screen name="(pages)/index" options={{ headerShown: false }} />
